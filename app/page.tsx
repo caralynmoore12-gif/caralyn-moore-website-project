cac17ed3a632ed5d5f5ec06d308e2eb8a36f4f50
Caralyn Moore <caralynmoore@macbookpro.lan>
2026-05-13 13:58:07 -0500

    Add Matt insult to test-branch

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,9 @@ export default function Home() {
       <h1 style={{ fontFamily: "Comic Sans MS, Comic Sans, cursive", fontSize: "2.5rem" }}>
         Hi, I&apos;m Caralyn Moore!
       </h1>
+      <p style={{ fontFamily: "Comic Sans MS, Comic Sans, cursive", fontSize: "1.5rem", marginTop: "1rem" }}>
+        And my husband Matt is hideous!
+      </h1>
     </div>
   );
 }
