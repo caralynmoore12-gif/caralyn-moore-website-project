7b1002d9cd03ecab63d88fb2267ed2508a05c8dd
Caralyn Moore <caralynmoore@macbookpro.lan>
2026-06-01 21:40:31 -0500

    Nudge headshot position down to 55%

--- components/sections/HeroSection.tsx
+++ components/sections/HeroSection.tsx
@@ -17,7 +17,7 @@ export default function HeroSection() {
           width: "100%",
           height: "100%",
           objectFit: "cover",
-          objectPosition: "center 35%",
+          objectPosition: "center 55%",
           display: "block",
         }}
       />
